cf8d421ceafac7640bd65f353bf59fc27c7ca99f
Cedric Kiplimo <kiplimock@gmail.com>
2023-03-27 09:16:58 +0300

    triggering button events and grabbing widget text

--- input.py
+++ input.py
@@ -27,7 +27,17 @@ class MyGrid(GridLayout):
         self.add_widget(self.inside)
 
         self.submit = Button(text="Submit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
         self.add_widget(self.submit)
+    
+    def pressed(self, instance):
+        first_name = self.first_name.text
+        last_name = self.last_name.text
+        email = self.email.text
+        print(f"First Name: {first_name}, Last Name: {last_name}, Email: {email}")
+        self.first_name.text = ""
+        self.last_name.text = ""
+        self.email.text = ""
 
 class MyApp(App):
     def build(self):
