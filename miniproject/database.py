c537678fcb237caa46662d3ca54066bfc0c57493
Cedric Kiplimo <kiplimock@gmail.com>
2023-03-29 18:42:20 +0300

    all three screens and transitions

---
+++ miniproject/database.py
@@ -0,0 +1,46 @@
+import datetime as dt
+
+class Database:
+    def __init__(self, filename):
+        self.filename = filename
+        self.users = None
+        self.file = None
+        self.load()
+
+    def load(self):
+        self.file = open(self.filename, 'r')
+        self.users = {}
+
+        for line in self.file:
+            email, password, username, created = line.strip().split(";")
+            self.users[email] = (password, username, created)
+        
+        self.file.close()
+    
+    def get_user(self, email):
+        if email in self.users:
+            return self.users[email]
+        return -1
+    
+    def add_user(self, email, password, username):
+        if email.strip() not in self.users:
+            self.users[email.strip()] = (password.strip(), username.strip(), Database.get_date())
+            self.save()
+            return 1
+        else:
+            print("Email already exists")
+            return -1
+    
+    def validate(self, email, password):
+        if self.get_user(email) != -1:
+            return self.users[email][0] == password
+        return False
+    
+    def save(self):
+        with open(self.filename, "w") as f:
+            for user in self.users:
+                f.write(user + ";" + self.users[user][0] + ";" + self.users[user][1] + ";" + self.users[user][2] + "\n")
+
+@staticmethod
+def get_date():
+    return str(dt.datetime.now()).split(" ")[0]
--- miniproject/main.py
+++ miniproject/main.py
@@ -4,16 +4,27 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.label  import Label
 from kivy.uix.popup import Popup
 from kivy.properties import ObjectProperty
+from database import Database
 
 
 class WindowManager(ScreenManager):
     pass
 
 sm = WindowManager()
+db = Database("miniproject/users.txt")
 
 def invalidForm():
     pop = Popup(
         title = "Invalid Form",
+        content = Label(text="Please fill in all inputs with valid information."),
+        size_hint = (None, None),
+        size = (400, 400),
+    )
+    pop.open()
+
+def invalidLogin():
+    pop = Popup(
+        title = "Invalid Login",
         content = Label(text="Invalid username or password."),
         size_hint = (None, None),
         size = (400, 400),
@@ -31,8 +42,7 @@ class CreateAccountWindow(Screen):
         if self.username.text == "" or self.email.text == "" or self.password.text == "":
             invalidForm()
         elif "@" in self.email.text and "." in self.email.text:
-            print("Cool")
-            # self.login()
+            self.login()
             
     # switch to the login screen
     def login(self):
@@ -45,9 +55,45 @@ class CreateAccountWindow(Screen):
         self.email.text = ""
         self.password.text = ""
 
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+    
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "login"
+
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+        self.created.text = "Created On: " + created
+
 kv = Builder.load_file("main.kv")
 
-screens = [CreateAccountWindow(name="create")]
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"),MainWindow(name="main")]
 for screen in screens:
     sm.add_widget(screen)
 
