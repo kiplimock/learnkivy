9a5e8a8902b7d92857394876006b8c8dcc856f8c
Cedric Kiplimo <kiplimock@gmail.com>
2023-04-18 15:00:15 +0300

    working with the grid layout

--- roux/the-lab/main.py
+++ roux/the-lab/main.py
@@ -3,6 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
 
 
 class AnchorLayoutExample(AnchorLayout):
