5e19bd318f5f9474902435b49aad9b3aa2ffb1c3
Cedric Kiplimo <kiplimock@gmail.com>
2023-04-18 14:44:36 +0300

    working with anchor layout

--- roux/the-lab/main.py
+++ roux/the-lab/main.py
@@ -2,7 +2,11 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
 
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
 
 class BoxLayoutExample(BoxLayout):
     pass
