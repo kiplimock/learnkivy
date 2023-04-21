9b3ebce808c5dcbaa05dcc5276899120a42a51c9
Cedric Kiplimo <kiplimock@gmail.com>
2023-04-21 17:43:29 +0300

    recentering the lines

--- roux/galaxy/main.py
+++ roux/galaxy/main.py
@@ -8,7 +8,7 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
     vertical_lines = []
-    V_NB_LINES = 7
+    V_NB_LINES = 8
     V_LINES_SPACING = 0.1
 
     def __init__(self, **kwargs):
@@ -34,31 +34,27 @@ class MainWidget(Widget):
         # print(f"PY: {value}")
         pass
 
-    # initiate vertical grid lines
     def init_vertical_lines(self):
         with self.canvas:
             Color(1,1,1)
-            # self.line = Line()
             for i in range(self.V_NB_LINES):
                 self.vertical_lines.append(Line())
 
-    # initiate horizontal grid lines
     def update_vertical_lines(self):
         central_line_x = int(self.width/2)
         spacing = self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES/2)
+        offset = -int(self.V_NB_LINES/2) + 0.5
         for i in range(self.V_NB_LINES):
             other_line_x = int(central_line_x + offset * spacing)
             x1, y1 = self.transform(other_line_x, 0)
             x2, y2 = self.transform(other_line_x, self.height)
 
             self.vertical_lines[i].points = [x1, y1, x2, y2]
-            print(self.vertical_lines[i].points)
             offset += 1
 
     def transform(self, x, y):
-        return self.transform_2D(x, y)
-        # return self.transform_perspective(x, y)
+        # return self.transform_2D(x, y)
+        return self.transform_perspective(x, y)
 
     def transform_2D(self, x, y):
         return int(x), int(y)
