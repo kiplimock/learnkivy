9e2b7863158b0913c9b97343230e82cf83f6c076
Cedric Kiplimo <kiplimock@gmail.com>
2023-04-21 15:54:14 +0300

    making the vertical lines

--- roux/galaxy/main.py
+++ roux/galaxy/main.py
@@ -1,14 +1,20 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
+from kivy.graphics import Color
+from kivy.graphics import Line
 
 class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
+    vertical_lines = []
+    V_NB_LINES = 11
+    V_LINES_SPACING = 0.1
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print(f"INIT W: {self.width} H: {self.height}")
+        self.init_vertical_lines()
     
     def on_parent(self, widget, parent):
         # print(f"ON PARENT W: {self.width} H: {self.height}")
@@ -18,7 +24,7 @@ class MainWidget(Widget):
         # print(f"ON SIZE W: {self.width} H: {self.height}")
         # self.perspective_point_x = self.width/2
         # self.perspective_point_y = self.height * 3/4
-        pass
+        self.update_vertical_lines()
     
     def on_perspective_point_x(self, widget, value):
         # print(f"PX: {value}")
@@ -27,6 +33,25 @@ class MainWidget(Widget):
     def on_perspective_point_y(self, widget, value):
         # print(f"PY: {value}")
         pass
+
+    # initiate vertical grid lines
+    def init_vertical_lines(self):
+        with self.canvas:
+            Color(1,1,1)
+            # self.line = Line()
+            for i in range(self.V_NB_LINES):
+                self.vertical_lines.append(Line())
+
+    # initiate horizontal grid lines
+    def update_vertical_lines(self):
+        central_line_x = int(self.width/2)
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES/2)
+        for i in range(self.V_NB_LINES):
+            other_line_x = int(central_line_x + offset * spacing)
+            self.vertical_lines[i].points = [other_line_x, 0, other_line_x, self.height]
+            print(self.vertical_lines[i].points)
+            offset += 1
 
 class GalaxyApp(App):
     pass
