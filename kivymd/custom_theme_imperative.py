28f9f3ee1da49632fb51099c27abe7f3d956ebc8
Cedric Kiplimo <kiplimock@gmail.com>
2023-05-01 18:33:05 +0300

    changing theme colors with imperative style

---
+++ kivymd/custom_theme_imperative.py
@@ -0,0 +1,72 @@
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
+from kivymd.app import MDApp
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.tab import MDTabsBase
+from kivymd.icon_definitions import md_icons
+
+colors = {
+    "Teal": {
+        "200": "#212121",
+        "500": "#212121",
+        "700": "#212121",
+    },
+    "Red": {
+        "200": "C25554",
+        "500": "C25554",
+        "700": "C25554",
+    },
+    "Light": {
+        "StatusBar": "#E0E0E0",
+        "AppBar": "#202020",
+        "Background": "2E3032",
+        "CardDialogs": "#FFFFFF",
+        "FlatButtonDown": "#CCCCCC",
+    },
+}
+
+KV = '''
+MDBoxLayout:
+    orientation: "vertical"
+
+    MDTopAppBar:
+        title: "Custom theme"
+    
+    MDTabs:
+        id: tabs
+
+<Tab>
+    MDIconButton:
+        id: icon
+        icon: root.icon
+        icon_size: "48sp"
+        theme_icon_color: "Custom"
+        icon_color: "white"
+        pos_hint: {"center_x": .5, "center_y": .5}
+'''
+
+class Tab(MDFloatLayout, MDTabsBase):
+    '''
+    Class implementing content for a tab
+    '''
+
+    icon = ObjectProperty()
+
+class Example(MDApp):
+    icons = list(md_icons.keys())[15:30]
+
+    def build(self):
+        self.theme_cls.colors = colors
+        self.theme_cls.primary_palette = "Teal"
+        self.theme_cls.accent_palette = "Red"
+        return Builder.load_string(KV)
+    
+    def on_start(self):
+        for name_tab in self.icons:
+            tab = Tab(title = f"This is {name_tab}", icon=name_tab)
+            self.root.ids.tabs.add_widget(tab)
+
+
+if __name__ == '__main__':
+    Example().run()
